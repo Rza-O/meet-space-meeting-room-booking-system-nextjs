794eb1045d6770b5550499dd894c09fd1c6b337d
appoclayptor <shahreza1414@gmail.com>
2025-02-14 00:44:59 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center">
+      <h2 className="text-3xl">Welcome to MeetSpace!</h2>
+      <p>Reserve your spot today.</p>
+    </div>
+  );
+}
