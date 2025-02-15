2cea16e0be740ca2fc3e1a74aedf4ccb93949413
appoclayptor <shahreza1414@gmail.com>
2025-02-16 00:48:17 +0600

    Add CRUD operations for room management; implement error handling and integrate sweetalert2 for notifications

--- src/app/api/rooms/route.ts
+++ src/app/api/rooms/route.ts
@@ -1,21 +1,74 @@
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-export const POST = async (req: NextRequest) => { 
-   try {
-      const {name, capacity, amenities, imageUrl} = await req.json();
-      const newRoom = await prisma.room.create({
-         data: { name, capacity, amenities, imageUrl },
-      });
-      return NextResponse.json(newRoom, {status: 201});
-   } catch (error) {
-      console.log("Error adding room:", error);
-      return NextResponse.json({ message: "Error adding room" },{ status: 500 });
-      
-   }
+export const POST = async (req: NextRequest) => {
+	try {
+		const { name, capacity, amenities, imageUrl } = await req.json();
+		const newRoom = await prisma.room.create({
+			data: { name, capacity, amenities, imageUrl },
+		});
+		return NextResponse.json(newRoom, { status: 201 });
+	} catch (error) {
+		console.log("Error adding room:", error);
+		return NextResponse.json(
+			{ message: "Error adding room" },
+			{ status: 500 }
+		);
+	}
 };
 
 export const GET = async () => {
-   const rooms = await prisma.room.findMany();
-   return NextResponse.json(rooms);
+	try {
+		const rooms = await prisma.room.findMany();
+		return NextResponse.json(rooms);
+	} catch (error) {
+		console.log(error);
+		return NextResponse.json(
+			{ message: "Error fetching rooms" },
+			{ status: 500 }
+		);
+	}
+};
+
+export const PATCH = async (
+	req: NextResponse,
+	{ params }: { params: { id: string } }
+) => {
+	try {
+		const roomId = params.id;
+		const body = await req.json();
+		// updating the room
+		const updatedRoom = await prisma.room.update({
+			where: { id: roomId },
+			data: body,
+		});
+		return NextResponse.json(updatedRoom, { status: 200 });
+	} catch (error) {
+		console.log(error);
+		return NextResponse.json(
+			{ message: "Error updating room" },
+			{ status: 500 }
+		);
+	}
+};
+
+export const DELETE = async (
+	req: NextRequest,
+	{ params }: { params: { id: string } }
+) => {
+   try {
+      const roomId = params.id;
+      // deleting the room
+      await prisma.room.delete({
+         where: {id: roomId},
+      })
+      return NextResponse.json({message: "Room deleted successfully"}, {status: 200});
+   } catch (error) {
+      console.log(error);
+      return NextResponse.json(
+         { message: "Error deleting room" },
+         { status: 500 }
+      ); 
+      
+   }
 };
